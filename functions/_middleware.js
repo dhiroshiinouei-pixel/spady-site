8a878dcea3f26e898e64720d6229af97a2f599a8
dhiroshiinouei-pixel <300093150+dhiroshiinouei-pixel@users.noreply.github.com>
2026-07-11 15:47:07 +0900

    Add Akindo landing page

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,18 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
+  let shouldRedirect = false;
 
   if (url.hostname === 'www.spady.net') {
     url.hostname = 'spady.net';
+    shouldRedirect = true;
+  }
+
+  if (url.pathname === '/akindo') {
+    url.pathname = '/akindo/';
+    shouldRedirect = true;
+  }
+
+  if (shouldRedirect) {
     return Response.redirect(url.toString(), 301);
   }
 
